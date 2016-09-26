f1ffeb6b2aced1e42894ad92d874ab9ee3d42406
Aixi Wang <aixi.wang@hotmail.com>
2016-09-26 14:12:43 +0800

    Add files via upload

--- ser2tcpserver.py
+++ ser2tcpserver.py
@@ -8,39 +8,34 @@ import argparse
 import logging
 import serial
 import sys
-
+import time
 
 class Ser2TcpClient():
     def __init__(self,host,port,serial_port,serial_baud):   
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock = s
         s.settimeout(10)
-        try:
-            s.connect((host, port))
-            
-        except:
-            sys.stdout.write('Unable to connect\n')
-            sys.stdout.flush()
-            sys.exit(1)
-        
+
+        s.connect((host, port))
+        print 'connect ', host, port, ' ok'
+
         self._serial = serial.Serial(
                 port=serial_port,
                 baudrate=serial_baud,
+                timeout = 1
             )
-
+        print 'open serial ',serial_port,' baud=',serial_baud, ' ok'
         
     def process(self):
+        print '.'
         buf = self._serial.read()
-        if len(buf) > 0:
-            self._sock.sendall(buf)
-        
-        
+        self._sock.sendall(buf)
 
 
 VERSION_STR = "ser2tcpserver v1.0.0"
 
 DESCRIPTION_STR = VERSION_STR + """
-(c) 2016 by aixi.wang@hotmail.com
+(c) 2016 by aixi.wang@gmail.com
 """
 
 def main():
@@ -64,22 +59,24 @@ def main():
     args = parser.parse_args()
 
 
-
-    ser2tcp = Ser2TcpClient(
-        args.server_ip,
-        args.server_port,
-        args.serial_port,
-        args.serial_baud
-    )
+    while True:
+        try:
     
-    try:
-        while True:
-            ser2tcp.process()
-    except Exception as err:
-        print("%s" % err)
-        raise err
-    except KeyboardInterrupt:
-        pass
+            ser2tcp = Ser2TcpClient(
+                args.server_ip,
+                args.server_port,
+                args.serial_port,
+                args.serial_baud
+            )
+            
+
+            while True:
+                ser2tcp.process()
+
+        except:
+            print 'wait 5 sec...'
+            time.sleep(5)
+        
 
 
 
